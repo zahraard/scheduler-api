1f9afa90433a712be6e6c77ef51116c0add62888
Zahra Vazifeh Ardalani <zahra.ardalani@gmail.com>
2020-10-07 18:39:01 -0400

    commit changes

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const server = require("http").Server(app);
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", socket => {
-  socket.onmessage = event => {
+wss.on("connection", (socket) => {
+  socket.onmessage = (event) => {
     console.log(`Message Received: ${event.data}`);
 
     if (event.data === "ping") {
@@ -24,11 +24,12 @@ function updateAppointment(id, interview) {
         JSON.stringify({
           type: "SET_INTERVIEW",
           id,
-          interview
+          interview,
         })
       );
     }
   });
+  
 }
 
 server.listen(PORT, () => {
